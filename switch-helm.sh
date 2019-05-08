87574cf2c01a5214c8d1a26df550d54811fb18ea
sotoiwa <sotoiwa@gmail.com>
2019-05-08 13:22:27 +0900

    switch-helm.sh

---
+++ switch-helm.sh
@@ -0,0 +1,32 @@
+/usr/local/bin/${COMMAND} version --client
+echo ""
+
+# 利用可能なバージョンを表示
+echo "Available ${COMMAND} version:"
+find -f /usr/local/bin/${COMMAND}* -type f
+echo ""
+
+# シンボリックリンクではないファイルについて繰り返し
+for v_command in $(find -f /usr/local/bin/${COMMAND}* -type f)
+do
+  while true
+  do
+    read -p "${v_command} <- use this version? (y/n/a):" yn
+    case $yn in
+      [yY]*)
+        rm -f /usr/local/bin/${COMMAND}
+        ln -s ${v_command} /usr/local/bin/helm
+        echo "${COMMAND} version changed!"
+        echo ""
+        echo "Current ${COMMAND} version:"
+        /usr/local/bin/${COMMAND} version --client
+        echo ""; echo "Done!"; exit ;;
+      [nN]*)
+        echo "Skip!"; echo ""; break ;;
+      [aA]*)
+        echo "Abort!"; echo ""; exit ;;
+    esac
+  done
+done
+echo "Done!"
+
